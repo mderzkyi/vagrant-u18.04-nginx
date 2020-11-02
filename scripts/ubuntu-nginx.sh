fd35b33312647504035336c58fd63bd0725e009e
mderzkyi <mderzkyi@gmail.com>
2020-11-02 20:54:23 +0200

    upgrade provision with git

--- scripts/ubuntu-nginx.sh
+++ scripts/ubuntu-nginx.sh
@@ -13,4 +13,8 @@ sudo chmod -R 755 /var/www
 rm -rf /var/www/html
 ln -s /vagrant /var/www/html
 
+cd /vagrant
+sudo -u vagrant wget -q https://raw.githubusercontent.com/mderzkyi/vagrant-u18.04-nginx/main/files/index.html
+
+
 sudo systemctl restart nginx
